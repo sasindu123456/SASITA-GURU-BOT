9611dc897a46c06e3fdd8ec0771ce79b01273567
sasindu123456 <140463672+sasindu123456@users.noreply.github.com>
2023-10-17 23:22:10 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94769607590', 'me', true],
+  ['94769607590', 'DARK SASI', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ DARK SASI'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
